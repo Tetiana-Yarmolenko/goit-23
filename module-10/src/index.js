41b0449ee5b86edc649d607f1651947c98328b58
a.Vienik <alexandrVenik@gmail.com>
2020-10-20 21:53:43 +0300

    add module-10

---
+++ module-10/src/index.js
@@ -0,0 +1,46 @@
+import './styles.scss';
+import 'swiper/swiper.scss';
+import Swiper from 'swiper';
+import scrollTo from 'scroll-to';
+import throttle from 'lodash.throttle';
+import slideTemplate from './templates/slide.hbs';
+import imagesList from './scripts/imageList';
+
+const imageListContainer = document.querySelector('.image-list');
+const scrollToButton = document.querySelector('.scroll-to-btn');
+const title = document.querySelector('.super-title');
+const slides = imagesList.reduce((acc, image) => {
+  return acc + slideTemplate(image)
+}, '');
+
+imageListContainer.innerHTML = slides;
+
+const mySwiper = new Swiper('.slider', {
+  speed: 400,
+  spaceBetween: 20,
+  slidesPerView: 3
+});
+
+scrollToButton.addEventListener('click', () => {
+  const topPosition = getElementScrollPosition(title);
+  const topMargin = 20;
+
+  scrollTo(0, topPosition - topMargin, {
+    ease: 'in-expo',
+    duration: 1500
+  });
+})
+
+const getElementScrollPosition = element => {
+  const { top, bottom, left, right } = element.getBoundingClientRect();
+  const windowScrollTopSize = window.scrollY;
+  console.log(`top: ${top}`);
+
+  return windowScrollTopSize + top;
+}
+
+const btnScrollDetection = throttle(() => {
+  getElementScrollPosition(scrollToButton)
+}, 300)
+
+// window.addEventListener('scroll', btnScrollDetection)
---
+++ module-10/src/scripts/imageList.js
@@ -0,0 +1,47 @@
+export default [
+  {
+    imgUrl: 'https://media.gettyimages.com/photos/young-man-at-sunset-picture-id496261146?s=612x612',
+    alt: 'super photo',
+    title: 'super photo',
+  },
+  {
+    imgUrl: 'https://www.bucksfreepress.co.uk/resources/images/11672462.jpg?display=1&htype=0&type=responsive-gallery',
+    alt: 'super photo',
+    title: 'super photo',
+  },
+  {
+    imgUrl: 'https://cdn-7.nikon-cdn.com/Images/Learn-Explore/Photography-Techniques/2019/CA-Chris-Ogonek-Picture-Controls/Media/Chris-Ogonek-Picture-Controls-Vivid.low.jpg',
+    alt: 'super photo',
+    title: 'super photo',
+  },
+  {
+    imgUrl: 'https://media.gettyimages.com/photos/young-man-at-sunset-picture-id496261146?s=612x612',
+    alt: 'super photo',
+    title: 'super photo',
+  },
+  {
+    imgUrl: 'https://www.bucksfreepress.co.uk/resources/images/11672462.jpg?display=1&htype=0&type=responsive-gallery',
+    alt: 'super photo',
+    title: 'super photo',
+  },
+  {
+    imgUrl: 'https://cdn-7.nikon-cdn.com/Images/Learn-Explore/Photography-Techniques/2019/CA-Chris-Ogonek-Picture-Controls/Media/Chris-Ogonek-Picture-Controls-Vivid.low.jpg',
+    alt: 'super photo',
+    title: 'super photo',
+  },
+  {
+    imgUrl: 'https://media.gettyimages.com/photos/young-man-at-sunset-picture-id496261146?s=612x612',
+    alt: 'super photo',
+    title: 'super photo',
+  },
+  {
+    imgUrl: 'https://www.bucksfreepress.co.uk/resources/images/11672462.jpg?display=1&htype=0&type=responsive-gallery',
+    alt: 'super photo',
+    title: 'super photo',
+  },
+  {
+    imgUrl: 'https://cdn-7.nikon-cdn.com/Images/Learn-Explore/Photography-Techniques/2019/CA-Chris-Ogonek-Picture-Controls/Media/Chris-Ogonek-Picture-Controls-Vivid.low.jpg',
+    alt: 'super photo',
+    title: 'super photo',
+  },
+]
